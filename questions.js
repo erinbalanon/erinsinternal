3f9a63d65e38ec6aa752e52031d9fc924bb46ead
erinbalanon <78568316+erinbalanon@users.noreply.github.com>
2021-11-08 09:29:44 +1300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,109 @@
+const questions = [
+    {
+      "question": "What do you enjoy doing in your free time?",
+      "answer1": "A. Reading books",
+      "answer1Total": "1",
+      "answer2": "B. Listening to music",
+      "answer2Total": "2",
+      "answer3": "C. Watching TV shows/movies",
+      "answer3Total": "3",
+          "answer4": "D. Going outside and doing activities",
+          "answer4Total": "4"
+    },
+  
+      {
+      "question": "When learning how to play a new board game I...",
+      "answer1": "A. Read the instructions manual",
+      "answer1Total": "1",
+      "answer2": "B. Listen to other people explain the rules and ask questions",
+      "answer2Total": "2",
+      "answer3": "C. Watch others play before joining in",
+      "answer3Total": "3",
+          "answer4": "D. Go straight to playing and learn by trial and error",
+          "answer4Total": "4"
+    },
+  
+      {
+      "question": "When studying, what would you normally do?",
+      "answer1": "A. Read articles and handouts",
+      "answer1Total": "1",
+      "answer2": "B. Talk things through",
+      "answer2Total": "2",
+      "answer3": "C. Look for patterns in things",
+      "answer3Total": "3",
+          "answer4": "D. use examples and applications",
+          "answer4Total": "4"
+    },
+  
+      {
+      "question": "How do you find your way to places?",
+      "answer1": "A. Writing down the directions",
+      "answer1Total": "1",
+      "answer2": "B. Ask around for directions",
+      "answer2Total": "2",
+      "answer3": "C. Using a map",
+      "answer3Total": "3",
+          "answer4": "D. Finding out the location using familiar surroundings",
+          "answer4Total": "4"
+    },
+  
+      {
+      "question": "When deciding on a study option or career, these things are important for me",
+      "answer1": "A. Writing down words to communicate your ideas",
+      "answer1Total": "1",
+      "answer2": "B. Communicating with others through discussions",
+      "answer2Total": "2",
+      "answer3": "C. Working with designs, graphs and charts",
+      "answer3Total": "3",
+          "answer4": "D. Applying knowledge in real life situations",
+          "answer4Total": "4"
+    },
+  
+      {
+      "question": "You are looking at houses/apartments/hotels. What do you do to find out more about the place?",
+      "answer1": "A. Read a description of the place",
+      "answer1Total": "1",
+      "answer2": "B. Discuss questions you have with the owner",
+      "answer2Total": "2",
+      "answer3": "C. Watch a video tour of the place",
+      "answer3Total": "3",
+          "answer4": "D. Go to the place and map out the area",
+          "answer4Total": "4"
+    },
+  
+      {
+      "question": "When getting feedback for something you've done, what do you prefer?",
+      "answer1": "A. A written description of your results",
+      "answer1Total": "1",
+      "answer2": "B. Discussing your results with someone",
+      "answer2Total": "2",
+      "answer3": "C. Using graphs and tables",
+      "answer3Total": "3",
+          "answer4": "D. Using examples from what you've done",
+          "answer4Total": "4"
+    },
+  
+      {
+      "question": "I prefer presenters who...",
+      "answer1": "A. Give out handouts and books",
+      "answer1Total": "1",
+      "answer2": "B. Do Q&As(question and answer) or group discussions",
+      "answer2Total": "2",
+      "answer3": "C. Use charts, maps or graphs",
+      "answer3Total": "3",
+          "answer4": "D. Use demonstrations, models or practical activities",
+          "answer4Total": "4"
+    },
+  
+      {
+      "question": "When trying to memorise a speech I...?",
+      "answer1": "A. Write it down",
+      "answer1Total": "1",
+      "answer2": "B. Listen to a recording of it",
+      "answer2Total": "2",
+      "answer3": "C. Picture the words or images that relate to it in your mind",
+      "answer3Total": "3",
+          "answer4": "D. Say it out loud",
+          "answer4Total": "4"
+    }
+  ]
